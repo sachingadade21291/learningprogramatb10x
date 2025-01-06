b8889c4446ce8a06435dacbfe8dbed616af9d761
sachin <sachingadade21291@gmail.com>
2025-01-06 23:13:17 +0530

    Third Commit

---
+++ src/practice/Lab001.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Lab001 {
+    public static void main(String[] args){
+        double h=10.5;
+        double g=2.5;
+        double j=h/g;
+        System.out.println(j);
+
+    }
+}

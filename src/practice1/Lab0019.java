06932a34982490f978097e743ad5cf97edfc9a5c
sachin <sachingadade21291@gmail.com>
2025-01-11 22:11:31 +0530

    commit

---
+++ src/practice1/Lab0019.java
@@ -0,0 +1,11 @@
+package practice1;
+
+public class Lab0019 {
+    public static void main(String[] args) {
+        float g=189.298f;
+        float h=22.89f;
+        float j=g/h;
+        System.out.println(j);
+
+    }
+}
---
+++ src/practice1/Lab0020.java
@@ -0,0 +1,10 @@
+package practice1;
+
+public class Lab0020 {
+    public static void main(String[] args) {
+        float h=20.23f;
+        float j=20.89f;
+        float k=h*j;
+        System.out.println(k);
+    }
+}

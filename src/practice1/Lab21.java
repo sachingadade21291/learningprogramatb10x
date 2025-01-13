458b9e55122e467786e20e0f11ac99fb9bab11d2
sachin <sachingadade21291@gmail.com>
2025-01-13 22:27:53 +0530

    commit

---
+++ src/practice1/Lab21.java
@@ -0,0 +1,14 @@
+package practice1;
+
+public class Lab21 {
+
+    public static void main(String [] args){
+        int age =28;
+        String name="Sachin";
+
+        System.out.printf("My name is %s",name);
+        System.out.println();
+        System.out.printf("My age is %d",age);
+
+    }
+}

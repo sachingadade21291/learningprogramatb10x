681b3d263c18d84b63786d461ad448d8dfd82bb4
sachin <sachingadade21291@gmail.com>
2025-01-13 22:34:35 +0530

    commit

---
+++ src/practice1/Lab22.java
@@ -0,0 +1,12 @@
+package practice1;
+
+public class Lab22 {
+    public static void main(String [] args){
+
+        float j=29.786f;
+        float h=898.77737f;
+        float k=j*h;
+        System.out.println(k);
+
+    }
+}

aa1ede80025e824eb6affbafdf63f1f4731fea98
sachin <sachingadade21291@gmail.com>
2025-01-14 23:28:52 +0530

    commit

---
+++ src/practice1/Lab0029.java
@@ -0,0 +1,13 @@
+package practice1;
+
+public class Lab0029 {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i*10);
+            i++;
+
+        }
+        while(i<=10);
+    }
+}

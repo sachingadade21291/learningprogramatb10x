eb3a0f8edb3b9a7d6223a1f86d69ff1ff7fbc877
sachin <sachingadade21291@gmail.com>
2025-01-15 23:04:16 +0530

    commit

---
+++ src/practice/Lab0032.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Lab0032 {
+    public static void main(String[] args) {
+        int i=20;
+        while(i>=1){
+            System.out.println(i);
+            i--;
+        }
+    }
+}

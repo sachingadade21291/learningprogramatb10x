c1e59747089e0035a07eeb3d60e2ddc21481f077
sachin <sachingadade21291@gmail.com>
2025-01-18 21:52:48 +0530

    commit

---
+++ src/practice2/Lab0034.java
@@ -0,0 +1,15 @@
+package practice2;
+
+public class Lab0034 {
+    public static void main(String []args){
+        int a=10;
+        int b=22;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(b%a);
+
+    }
+
+}

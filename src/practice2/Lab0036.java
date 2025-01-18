f0cdc56098aedba1d06e406ff0a436523b04ce9b
sachin <sachingadade21291@gmail.com>
2025-01-18 22:11:42 +0530

    commit

---
+++ src/practice2/Lab0036.java
@@ -0,0 +1,13 @@
+package practice2;
+
+public class Lab0036 {
+    public static void main(String[] args) {
+        boolean k=true;
+        boolean j=true;
+
+        System.out.println(j);
+        System.out.println(k);
+
+
+    }
+}

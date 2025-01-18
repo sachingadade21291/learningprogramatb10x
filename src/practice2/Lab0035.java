aee52219262a4e9eef33e5a466632b811a0b5d47
sachin <sachingadade21291@gmail.com>
2025-01-18 22:04:56 +0530

    commit

---
+++ src/practice2/Lab0035.java
@@ -0,0 +1,13 @@
+package practice2;
+
+public class Lab0035 {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==10);
+        System.out.println(a>=10);
+        System.out.println(b<=20);
+    }
+}

6e430ec019a5bd3ad40ed51d449225eb22055a5d
sachin <sachingadade21291@gmail.com>
2025-01-19 22:03:34 +0530

    commit

---
+++ src/practice3/Increament.java
@@ -0,0 +1,11 @@
+package practice3;
+
+public class Increament {
+    public static void main(String []args){
+
+        int a=20;
+        System.out.println(--a + a++ +a--);
+        System.out.println(a);
+    }
+
+}
---
+++ src/practice3/Userinput.java
@@ -0,0 +1,15 @@
+package practice3;
+
+public class Userinput {
+    public static void main(String[] args) {
+        String are=args[0];
+        int age=Integer.parseInt(are);
+        System.out.println(age);
+        String Name=args[1];
+        String jre=args[2];
+        int Salary= Integer.parseInt(jre);
+        System.out.println(age);
+        System.out.println(Name);
+        System.out.println(Salary);
+    }
+}

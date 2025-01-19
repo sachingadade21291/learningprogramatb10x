d428893ed714e57938a9f1b0dc00676a5336e788
sachin <sachingadade21291@gmail.com>
2025-01-19 16:13:47 +0530

    commit

---
+++ src/practice3/Largestno.java
@@ -0,0 +1,13 @@
+package practice3;
+
+public class Largestno {
+    public static void main(String[] args) {
+        int n1=23;
+        int n2=49;
+        int n3=5;
+        int max= n1>n2? n1:n2;
+        int Largest= max>n3? max:n3;
+        System.out.println(Largest);
+
+    }
+}

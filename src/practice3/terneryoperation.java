d2b6c2aea1bc78f31d0962297c1cc69c2655c138
sachin <sachingadade21291@gmail.com>
2025-01-21 22:16:56 +0530

    commit

---
+++ src/practice3/terneryoperation.java
@@ -0,0 +1,20 @@
+package practice3;
+
+public class terneryoperation {
+
+    public static void main(String[] args){
+
+        boolean h=true;
+        boolean j=false;
+        System.out.println(h||j);
+        System.out.println(h&&j);
+        System.out.println(!h||j);
+        System.out.println(!h||!j);
+        System.out.println(h||!j);
+        System.out.println(!h&&j);
+        System.out.println(!h&&!j);
+        System.out.println(h&&!j);
+        System.out.println (!(!h&&!j));
+
+    }
+}
